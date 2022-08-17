a9e80687445b6fea4016b34c38e83742342540c1
Nikolay <nkspb13@gmail.com>
2022-08-17 18:24:17 +0300

    Create check_md5.sh

---
+++ check_md5.sh
@@ -0,0 +1,9 @@
+REPO_MD5=$(curl -sL https://raw.githubusercontent.com/nkspb/webapp/main/html/index.html | md5sum | cut -d ' ' -f 1)
+LOCAL_MD5=$(curl -sL http://localhost:9889/index.html | md5sum | cut -d ' ' -f 1)
+
+if [ $REPO_MD5 = $LOCAL_MD5 ]
+  then
+    echo "##teamcity[message text='md5 check succeeded' status='NORMAL']"
+  else
+    echo "##teamcity[message text='md5 check failed' errorDetails='md5 of repo and local index.html are different' status='ERROR']"
+fi
