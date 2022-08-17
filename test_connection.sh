8728778f97daec0b5188edc17237c158818d1cea
Nikolay <nkspb13@gmail.com>
2022-08-17 18:22:57 +0300

    Update test_connection.sh

--- test_connection.sh
+++ test_connection.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 STATUS_CODE=$(curl -LI http://localhost:9889 -o /dev/null -w '%{http_code}\n' -s)
 
 if [ $STATUS_CODE -eq 200 ]
   then
-    echo "##teamcity[message text='connection_test_result' errorDetails='Connection to webapp on http succeeded' status='NORMAL']"
+    echo "##teamcity[message text='Connection succeeded' status='NORMAL']"
   else
-    echo "##teamcity[message text='connection_test_result' errorDetails='Connection to webapp on http failed' status='ERROR']"
+    echo "##teamcity[message text='Connection failed' errorDetails='Connection to webapp on http failed' status='ERROR']"
 fi
